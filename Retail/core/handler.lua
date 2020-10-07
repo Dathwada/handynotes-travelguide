b5485545008cafa85026c718f4b8744193f82866
Dathwada <Dathwada@users.noreply.github.com>
2020-10-07 16:11:32 +0200

    Fix spell requirement

--- Retail/core/handler.lua
+++ Retail/core/handler.lua
@@ -235,7 +235,7 @@ end
         end
         if (point.npc) then
             local spellName = GetSpellInfo(point.spell)
-            if point.spell and point.npc == true then
+            if point.spell and point.npc then
                 tooltip:AddLine(spellName)
             else
                 tooltip:SetHyperlink(("unit:Creature-0-0-0-0-%d"):format(point.npc))
@@ -275,9 +275,10 @@ end
                 end
             end
         end
-        if (point.spell and not point.timetravel and not point.npc == true) then
+        if (point.spell and not point.timetravel and not point.npc) then -- don't show this if the spell is known
             local spellName = GetSpellInfo(point.spell)
-            if spellName then
+            local isKnown = IsSpellKnown(point.spell)
+            if spellName and not isKnown then
                 tooltip:AddLine(requires..': '..spellName, 1) -- red
             end
         end
