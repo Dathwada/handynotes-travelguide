f2a7e30394263cc49586b582d9f6f6276866896b
Dathwada <Dathwada@users.noreply.github.com>
2020-10-12 03:26:54 +0200

    Add French (frFR)
    created with wow.tools and ingame reviews

---
+++ Retail/localization/frFR.lua
@@ -0,0 +1,269 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_TravelGuide", "frFR", false, true)
+
+if not L then return end
+--French translation by  ( https://www.curseforge.com/members/ )
+if L then
+
+L["Anima Gateway"] = "Anima Gateway"
+
+L["Anima Gateway to Hero's rest"] = "Anima Gateway to Hero's rest"
+L["Ardenweald"] = "Sylvarden"
+L["Azsuna"] = true
+L["Bastion"] = "Le Bastion"
+L["Boat to Booty Bay"] = "Bateau vers Baie-du-Butin"
+L["Boat to Boralus"] = "Bateau vers Boralus"
+L["Boat to Borean Tundra"] = "Bateau vers Toundra Boréenne"
+L["Boat to Drustvar"] = "Bateau vers Drustvar"
+L["Boat to Exodar"] = "Bateau vers L’Exodar"
+L["Boat to Howling Fjord"] = "Bateau vers Fjord Hurlant"
+L["Boat to Kamagua"] = "Bateau vers Kamagua"
+L["Boat to Menethil Harbor"] = "Bateau vers Port de Menethil"
+L["Boat to Moa'Ki Harbor"] = "Bateau vers Port-Moa’ki"
+L["Boat to Nazmir"] = "Bateau vers Nazmir"
+L["Boat to Ratchet"] = "Bateau vers Ratchet"
+L["Boat to Stormsong Valley"] = "Bateau vers Vallée Chantorage"
+L["Boat to Stormwind"] = "Bateau vers Hurlevent"
+L["Boat to Theramore"] = "Bateau vers Theramore"
+L["Boat to Tiragarde Sound"] = "Bateau vers Rade de Tiragarde"
+L["Boat to Unu'Pe"] = "Bateau vers Unu’pe"
+L["Boat to Vol'Dun"] = "Bateau vers Vol’dun"
+L["Boat to Zuldazar"] = "Bateau vers Zuldazar"
+L["Broken Isles"] = "Îles Brisées"
+L["Broken Isles, Stormheim"] = "Îles Brisées, Tornheim"
+L["Broken Shore"] = "Rivage Brisé"
+
+L["config_anima_gateway"] = "Anima Gateways in Bastion"
+
+L["config_anima_gateway_desc"] = "Show the anima gateway locations in Bastion."
+L["config_boat"] = "Bateau"
+L["config_boat_alliance"] = "Bateau de l'Alliance"
+L["config_boat_alliance_desc"] = "Afficher locations des bateaux de l'Alliance."
+L["config_boat_desc"] = "Afficher locations des bateaux"
+
+L["config_covenant"] = "Covenant Travel Network"
+
+L["config_covenant_desc"] = "Show locations of access points for the covenant travel network."
+L["config_deeprun_tram"] = "Tram des Profondeurs"
+L["config_deeprun_tram_desc"] = "Montrez les emplacements des trams de profondeurs à Stormwind et Ironforge."
+
+L["config_easy_waypoints"] = "Easy waypoints"
+
+L["config_easy_waypoints_desc"] = "Activates simplified waypoint creation. \nAllows you to set a waypoint by right-clicking and access to more options by CTRL + right-clicking."
+L["config_icon_alpha"] = "Opacité"
+L["config_icon_alpha_desc"] = "La transparence des icônes"
+L["config_icon_scale"] = "Échelle"
+L["config_icon_scale_desc"] = "L'échelle des icônes"
+L["config_note"] = "Note"
+L["config_note_desc"] = "Affiche les notes supplémentaires du nœud, lorsqu'il est disponible."
+
+L["config_order_hall_portal"] = "Order Hall portal"
+
+L["config_order_hall_portal_desc"] = "Show the Order Hall portal locations."
+
+L["config_others"] = "Others"
+
+L["config_others_desc"] = "Show all the other POIs."
+
+L["config_plugin_desc"] = "Shows the portal, zeppelin and boat locations on the World Map and the MiniMap."
+L["config_portal"] = "Portail"
+
+L["config_portal_desc"] = "Show the portal locations."
+
+L["config_restore_nodes"] = "Restore hidden nodes"
+
+L["config_restore_nodes_desc"] = "Restore all nodes that were hidden via the context menu."
+
+L["config_restore_nodes_print"] = "All hidden nodes have been restored"
+L["config_tab_general"] = "Général"
+L["config_tab_scale_alpha"] = "Échelle / Opacité"
+
+L["config_teleport_platform"] = "Teleport platforms in Oribos"
+
+L["config_teleport_platform_desc"] = "Show the teleport plattform locations in Oribos."
+
+L["config_warfront_portal"] = "Warfront portal"
+
+L["config_warfront_portal_desc"] = "Show the Warfront portal locations."
+L["config_what_to_display"] = "Quoi afficher?"
+L["config_what_to_display_desc"] = "Ces paramètres contrôlent le type d’icônes à afficher."
+L["config_zeppelin"] = "Zeppelin"
+L["config_zeppelin_desc"] = "Afficher locations des Zepplins."
+L["config_zeppelin_horde"] = "Zepplin de Horde"
+L["config_zeppelin_horde_desc"] = "Montrez les emplacements des Zeppelin de la Horde."
+L["Dalaran"] = true
+L["Darkreach"] = "Sombres-Confins"
+L["Deeprun Tram to Ironforge"] = "Tram des Profondeurs vers Ironforge"
+L["Deeprun Tram to Stormwind"] = "Tram des Profondeurs vers Stormwind"
+L["Dominance Keep"] = "Donjon de la Domination"
+L["Draenor, Ashran"] = "Draenor, A’shran"
+L["Draenor, Tanaan Jungle"] = "Draenor, Jungle de Tanaan"
+L["Eastern Kingdoms"] = "Royaumes de l’Est"
+L["Eastern Kingdoms, Arathi Highlands"] = "Royaumes de l’Est, Arathi Highlands"
+L["Eastern Kingdoms, Dun Morogh"] = "Royaumes de l’Est, Dun Morogh"
+L["Eastern Kingdoms, Elwynn Forest"] = "Royaumes de l’Est, Forêt d’Elwynn"
+L["Eastern Kingdoms, Eversong Woods"] = "Royaumes de l’Est, Bois des Chants éternels"
+L["Eastern Kingdoms, Grom'gol Base Camp"] = "Royaumes de l’Est, Campement Grom’gol"
+L["Eastern Kingdoms, Hillsbrad Foothills"] = "Royaumes de l’Est, Contreforts de Hautebrande"
+L["Eastern Kingdoms, Silverpine Forest"] = "Royaumes de l’Est, Forêt des Pins-Argentés"
+L["Eastern Kingdoms, Stranglethorn Vale"] = "Royaumes de l’Est, Vallée de Strangleronce"
+L["Eastern Kingdoms, Tirisfal Glades"] = "Royaumes de l’Est, Clairières de Tirisfal"
+L["Eastern Kingdoms, Wetlands"] = "Royaumes de l’Est, Les Paluns"
+L["Ember Ward"] = "Province des Braises"
+
+L["Eternal Gateway"] = "Eternal Gateway"
+L["Feeders' Thicket"] = "Fourré des Dévoreurs"
+
+L["Great Eagle to Trueshot Lodge"] = "Great Eagle to Trueshot Lodge"
+L["Halls of Atonement"] = "Salles de l’Expiation"
+L["handler_context_menu_add_tomtom"] = "Ajouter à TomTom"
+L["handler_context_menu_hide_node"] = "Cacher ce nœud"
+
+L["handler_tooltip_data"] = "RETRIEVING DATA..."
+
+L["handler_tooltip_not_available"] = "currently NOT available"
+
+L["handler_tooltip_quest"] = "Unlocked with quest"
+L["handler_tooltip_requires"] = "Nécessite"
+
+L["handler_tooltip_requires_level"] = "Requires at least player level"
+
+L["handler_tooltip_sanctum_feature"] = "a Sanctum Upgrade"
+
+L["handler_tooltip_TNTIER"] = "Tier %s of the travel network."
+L["Heartwood Grove"] = "Bosquet du Xylocarde"
+L["Highmountain"] = "Haut-Roc"
+
+L["in Exodar"] = "in Exodar"
+
+L["in Undercity Magic Quarter"] = "in Undercity Magic Quarter"
+
+L["Into the Maw"] = "Into the Maw"
+
+L["Jump to Skyhold"] = "Jump to Skyhold"
+L["Kalimdor"] = true
+L["Kalimdor, Azuremyst Isle"] = "Kalimdor, Île de Brume-Azur"
+L["Kalimdor, Darkshore"] = "Kalimdor, Sombrivage"
+L["Kalimdor, Durotar"] = true
+L["Kalimdor, Dustwallow Marsh"] = "Kalimdor, Marécage d’Âprefange"
+L["Kalimdor, Mulgore"] = true
+L["Kalimdor, Northern Barrens"] = "Kalimdor, Tarides du Nord"
+L["Kalimdor, Tanaris"] = true
+L["Kalimdor, Teldrassil"] = true
+L["Kul Tiras"] = true
+L["Kul Tiras, Tiragarde Sound"] = "Kul Tiras, Rade de Tiragarde"
+L["Maelstrom"] = "Le Maelström"
+L["Maelstrom, Deepholm"] = "Le Maelström, Le Tréfonds"
+
+L["Main Hall, Seat of the Primus"] = "Main Hall, Seat of the Primus"
+L["Maldraxxus"] = true
+
+L["near Dreamsang Fenn"] = "near Dreamsang Fenn"
+
+L["near Hibernal Hollow"] = "near Hibernal Hollow"
+
+L["near Tirna Scithe"] = "near Tirna Scithe"
+
+L["near Tirna Vaal"] = "near Tirna Vaal"
+L["Northrend, Borean Tundra"] = "Norfendre, Toundra Boréenne"
+L["Northrend, Crystalsong Forest"] = "Norfendre, Forêt du Chant de cristal"
+L["Northrend, Dragonblight"] = "Norfendre, Désolation des dragons"
+L["Northrend, Howling Fjord"] = "Norfendre, Fjord Hurlant"
+L["Northrend, Valgarde"] = "Norfendre, Valgarde"
+L["Northrend, Valiance Keep"] = "Norfendre, Donjon de la Bravoure"
+L["Northrend, Vengeance Landing"] = "Norfendre, Accostage de la Vengeance"
+L["Northrend, Warsong Hold"] = "Norfendre, Bastion Chanteguerre"
+
+L["Nurakkir, House of Eyes"] = "Nurakkir, House of Eyes"
+L["Orb of translocation"] = "Orbe de transposition"
+L["Oribos"] = true
+L["Outland"] = "Outreterre"
+L["Outland, Terokkar Forest"] = "Outreterre, Forêt de Terokkar"
+
+L["Overlook, Seat of the Primus"] = "Overlook, Seat of the Primus"
+L["Pandaria"] = "Pandarie"
+L["Pandaria, Kun-Lai Summit"] = "Pandarie, Sommet de Kun-Lai"
+L["Pandaria, Townlong Steppes"] = "Pandarie, Steppes de Tanglong"
+L["Portal to Arathi Highlands"] = "Portail vers les hautes-terres Arathies"
+L["Portal to Azsuna"] = "Portail vers Azsuna"
+L["Portal to Boralus"] = "Portail vers Boralus"
+L["Portal to Caverns of Time"] = "Portail vers les Grottes du temps"
+L["Portal to Dalaran"] = "Portail vers Dalaran"
+L["Portal to Dalaran Crater"] = "Portail vers le cratère de Dalaran"
+L["Portal to Darkshore"] = "Portail vers Sombrivage"
+L["Portal to Darnassus"] = "Portail vers Darnassus"
+L["Portal to Deepholm"] = "Portail vers le Tréfonds"
+L["Portal to Emerald Dreamway"] = "Portail vers Chemin du Rêve d’émeraude"
+L["Portal to Exodar"] = "Portail vers L'Exodar"
+L["Portal to Helheim"] = "Portail vers Helheim"
+L["Portal to Hellfire Peninsula"] = "Portail vers la péninsule des Flammes infernales"
+L["Portal to Highmountain"] = "Portail vers Haut-Roc"
+L["Portal to Howling Fjord"] = "Portail vers le fjord Hurlant"
+L["Portal to Hyjal"] = "Portail vers Hyjal"
+L["Portal to Ironforge"] = "Portail vers Forgefer"
+L["Portal to Isle of Quel'Danas"] = "Shattrath, portail pour I'île de Quel'Danas"
+L["Portal to Isle of Thunder"] = "Portail vers I'île du Tonnerre"
+L["Portal to Jade Forest"] = "Portail vers la forêt de Jade"
+L["Portal to Lion's watch"] = "Portail vers le guet du Lion"
+L["Portal to Nazjatar"] = "Portail vers Nazjatar"
+L["Portal to Orgrimmar"] = "Portail vers Orgrimmar"
+L["Portal to Oribos"] = "Portail vers Oribos"
+L["Portal to Peak of Serenity"] = "Portail vers le pic de la Sérénité"
+L["Portal to Port of Boralus"] = "Portail vers le port de Boralus"
+L["Portal to Port of Zandalar"] = "Portail vers le port de Zandalar"
+L["Portal to Shado-Pan Garrison"] = "Portail vers la garnison des Pandashan"
+L["Portal to Shattrath"] = "Portail vers Shattrath"
+L["Portal to Silithus"] = "Portail vers Silithus"
+L["Portal to Silvermoon"] = "Portail vers Lune-d'Argent"
+L["Portal to Stormheim"] = "Portail vers Tornheim"
+L["Portal to Stormshield"] = "Portail vers Bouclier-des-Tempêtes"
+L["Portal to Stormwind"] = "Portail vers Hurlevent"
+L["Portal to Stranglethorn Vale"] = "Portail vers la vallée de Strangleronce"
+L["Portal to Suramar"] = "Portail vers Suramar"
+L["Portal to Temple of Earth"] = "Portail vers le temple de la Terre"
+L["Portal to the Purple Parlor"] = "Portail vers le salon Violet"
+L["Portal to the Sepulcher"] = "Portail vers le Sépulcre"
+L["Portal to Therazane's Throne"] = "Portail vers le Trône de Therazane"
+L["Portal to Thorghast"] = "Portail vers Thorghast"
+L["Portal to Thunder Bluff"] = "Portail vers les Pitons-du-Tonnerre"
+L["Portal to Tol Barad"] = "Portail vers Tol Barad"
+L["Portal to Twilight Highlands"] = "Portail vers les hautes-terres du Crépuscule"
+L["Portal to Uldum"] = "Portail vers Uldum"
+L["Portal to Undercity"] = "Portail vers Fossoyeuse"
+L["Portal to Val'sharah"] = "Portail vers Val'sharah"
+L["Portal to Vashj'ir"] = "Portail vers Vashj'ir"
+L["Portal to Vol'mar"] = "Portail vers Vol'mar"
+L["Portal to Warspear"] = "Portail vers Fer-de-Lance"
+L["Portal to Zuldazar"] = "Portail vers Zuldazar"
+L["Pridefall Hamlet"] = "Tombe-Fierté"
+
+L["RANDOM LOCATION"] = "RANDOM LOCATION"
+L["Reavendreath"] = true
+
+L["Return to Boralus"] = "Return to Boralus"
+
+L["Return to Zuldazar"] = "Return to Zuldazar"
+L["Sanctuary of the Mad"] = "Sanctuaire du Fou"
+L["Seat of the Primus"] = "Siège du Primat"
+L["Shadowlands"] = "Ombreterre"
+L["Shimmerbough"] = "Lumefronde"
+L["Sinfall"] = "Tombe-Vice"
+L["Stormheim"] = "Tornheim"
+
+L["Submarine to Mechagon"] = "Submarine to Mechagon"
+L["Suramar"] = true
+L["The Banewood"] = "Le Malbois"
+L["The Eternal Terrace"] = "Terrasse Éternelle"
+L["The Maw"] = "Antre"
+L["To Ring of Fates"] = "Vers I'anneau des destins"
+L["To Ring of Transference"] = "Vers I'anneau de transfert"
+L["Val'sharah"] = true
+L["Waystone to Oribos"] = "Pierre d'accès à Oribos"
+L["Zandalar"] = true
+L["Zandalar, Zuldazar"] = true
+L["Zeppelin to Borean Tundra"] = "Zeppelin vers Toundra Boréenne"
+L["Zeppelin to Orgrimmar"] = "Zeppelin vers Orgrimmar"
+L["Zeppelin to Stranglethorn Vale"] = "Zeppelin vers Vallée de Strangleronce"
+L["Zeppelin to Thunder Bluff"] = "Zeppelin vers Pitons-du-Tonnerre"
+
+L["Zerekriss: Maldraxxus"] = "Zerekriss: Maldraxxus"
+end
