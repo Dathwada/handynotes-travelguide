4bbabee045745e354fe5c53d0f50930c3804b4ff
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2022-06-07 19:46:49 +0800

    Update zhCN.lua

--- Retail/localization/zhCN.lua
+++ Retail/localization/zhCN.lua
@@ -2,126 +2,247 @@ local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_TravelGuide", "zhCN", f
 
 if not L then return end
 -- Simplified Chinese translation by mmk5 ( https://www.curseforge.com/members/mmk5 )
+--  zhCN client: (NGA-男爵凯恩)
+--  Last update: 2022/06/07
 if L then
-L["Boat to Booty Bay"] = "前往藏宝海湾的船"
-L["Boat to Boralus"] = "前往伯拉勒斯的船"
-L["Boat to Borean Tundra"] = "前往北风苔原的船"
-L["Boat to Drustvar"] = "前往德鲁斯瓦的船"
-L["Boat to Exodar"] = "前往埃索达的船"
-L["Boat to Howling Fjord"] = "前往嚎风峡湾的船"
-L["Boat to Kamagua"] = "前往卡玛古（嚎风峡湾）的船"
-L["Boat to Menethil Harbor"] = "前往米奈希尔港（湿地）的船"
-L["Boat to Moa'Ki Harbor"] = "前往莫亚基港口（龙骨荒野）的船"
-L["Boat to Nazmir"] = "前往纳兹米尔的船"
-L["Boat to Ratchet"] = "前往棘齿城（北贫瘠之地）的船"
-L["Boat to Stormsong Valley"] = "前往斯托颂谷地的船"
-L["Boat to Stormwind"] = "前往暴风城的船"
-L["Boat to Theramore"] = "前往塞拉摩的船"
-L["Boat to Tiragarde Sound"] = "前往提拉加德海峡的船"
-L["Boat to Unu'Pe"] = "前往乌努比（北风苔原）的船"
-L["Boat to Vol'Dun"] = "前往沃顿的船"
-L["Boat to Zuldazar"] = "前往祖达萨的船"
-L["config_boat"] = "船"
-L["config_boat_alliance"] = "船-联盟"
-L["config_boat_alliance_desc"] = "显示所有联盟船只的位置。"
-L["config_boat_desc"] = "显示船的位置。"
-L["config_easy_waypoints"] = "简易导航点"
-L["config_easy_waypoints_desc"] = "创建简易导航点。\n右键点击设置导航，CTRL + 右键更多选项。"
-L["config_icon_alpha"] = "图标透明度"
-L["config_icon_alpha_desc"] = "图标透明度"
-L["config_icon_scale"] = "图标缩放"
-L["config_icon_scale_desc"] = "图标缩放"
-L["config_note"] = "图标"
-L["config_note_desc"] = "当图标（船/传送点）可用时，显示相关的注释。"
-L["config_order_hall_portal"] = "职业大厅"
-L["config_order_hall_portal_desc"] = "显示职业大厅传送门。"
-L["config_others"] = "其它"
-L["config_others_desc"] = "显示所有其它POI。"
-L["config_plugin_desc"] = "在世界地图和小地图上显示传送门、飞艇、港口图标。"
 L["config_plugin_name"] = "旅行指南"
-L["config_portal"] = "传送门"
-L["config_portal_desc"] = "显示传送门位置。"
-L["config_restore_nodes"] = "回复所有隐藏图标"
-L["config_restore_nodes_desc"] = "显示所有被你隐藏的图标。"
-L["config_restore_nodes_print"] = "所有隐藏图标已还原"
-L["config_tab_general"] = "一般"
-L["config_tab_scale_alpha"] = "缩放 / 透明度"
-L["config_tram"] = "矿道地铁"
-L["config_tram_desc"] = "显示暴风城和铁炉堡的矿道地铁位置。"
-L["config_warfront_portal"] = "战争前线传送门"
-L["config_warfront_portal_desc"] = "显示战争前线传送门。"
+L["config_plugin_desc"] = "在世界地图和小地图上显示传送门、飞艇、港口图标。"
+
+L["config_tab_general"] = "通用"
+L["config_tab_scale_alpha"] = "缩放/透明度"
+--L["config_scale_alpha_desc"] = "PH"
+L["config_icon_scale"] = "图标大小"
+L["config_icon_scale_desc"] = "调整图标的大小"
+L["config_icon_alpha"] = "图标透明度"
+L["config_icon_alpha_desc"] = "修改图标透明度"
 L["config_what_to_display"] = "显示什么？"
 L["config_what_to_display_desc"] = "在这里设置显示哪些类型的图标。"
+
+L["config_portal"] = "传送门"
+L["config_portal_desc"] = "显示传送门位置。"
+
+L["config_order_hall_portal"] = "职业大厅"
+L["config_order_hall_portal_desc"] = "显示职业大厅传送门。"
+
+L["config_warfront_portal"] = "战争前线传送门"
+L["config_warfront_portal_desc"] = "显示战争前线传送门。"
+
+L["config_petbattle_portal"] = "宠物对战传送门"
+L["config_petbattle_portal_desc"] = "显示宠物对战传送门"
+
+L["config_ogreWaygate"] = "食人魔传送门"
+L["config_ogreWaygate_desc"] = "显示食人魔传送门"
+
+L["config_boat"] = "船"
+L["config_boat_desc"] = "显示船的位置。"
+L["config_boat_alliance"] = "船-联盟"
+L["config_boat_alliance_desc"] = "显示所有联盟船只的位置。"
+
 L["config_zeppelin"] = "飞艇"
 L["config_zeppelin_desc"] = "显示飞艇位置。"
 L["config_zeppelin_horde"] = "飞艇-部落"
 L["config_zeppelin_horde_desc"] = "显示所有部落飞艇的位置。"
-L["Deeprun Tram to Ironforge"] = "前往铁炉堡的矿道地铁"
-L["Deeprun Tram to Stormwind"] = "前往暴风城的矿道地铁"
-L["Great Eagle to Trueshot Lodge"] = "前往神射手营地的巨鹰"
+
+L["config_tram"] = "矿道地铁"
+L["config_tram_desc"] = "显示暴风城和铁炉堡的矿道地铁位置。"
+
+L["config_note"] = "图标"
+L["config_note_desc"] = "当图标（船/传送点）可用时，显示相关的注释。"
+
+L["config_easy_waypoints"] = "简易导航点"
+L["config_easy_waypoints_desc"] = "创建简易导航点。\n右键点击设置导航，CTRL + 右键更多选项。" 
+
+L["config_teleportPlatform"] = "奥利波斯传送平台"
+L["config_teleportPlatform_desc"] = "显示奥利波斯传送平台位置."
+
+L["config_animaGateway"] = "显示心能传送门"
+L["config_animaGateway_desc"] = "显示心能传送门位置。."
+
+L["config_others"] = "其它"
+L["config_others_desc"] = "显示所有其它POI。"
+
+L["config_restore_nodes"] = "恢复所有隐藏图标"
+L["config_restore_nodes_desc"] = "显示所有被你隐藏的图标。"
+L["config_restore_nodes_print"] = "所有隐藏图标已还原"
+
+----------------------------------------------------------------------------------------------------
+-------------------------------------------------DEV------------------------------------------------
+----------------------------------------------------------------------------------------------------
+
+L["dev_config_tab"] = "DEV"
+
+L["dev_config_force_nodes"] = "强制显示"
+L["dev_config_force_nodes_desc"] = "无论你的职业或阵营, 强制显示所有的点."
+
+L["dev_config_show_prints"] = "显示 print()"
+L["dev_config_show_prints_desc"] = "在聊天窗口中显示 print() 的信息."
+
+----------------------------------------------------------------------------------------------------
+-----------------------------------------------HANDLER----------------------------------------------
+----------------------------------------------------------------------------------------------------
+
+--==========================================CONTEXT_MENU==========================================--
+
+L["handler_context_menu_addon_name"] = "HandyNotes: 旅行指南"
 L["handler_context_menu_add_tomtom"] = "添加到TomTom"
 L["handler_context_menu_hide_node"] = "隐藏图标"
-L["handler_tooltip_data"] = "接收数据中..."
-L["handler_tooltip_not_available"] = "目前不可用"
-L["handler_tooltip_quest"] = "需要解锁任务"
+
+--============================================TOOLTIPS============================================--
+
 L["handler_tooltip_requires"] = "需要"
+L["handler_tooltip_sanctum_feature"] = "圣所升级"
+L["handler_tooltip_data"] = "接收数据中..."
+L["handler_tooltip_quest"] = "需要解锁任务"
 L["handler_tooltip_requires_level"] = "需要玩家等级"
-L["in Exodar"] = "埃索达内部"
-L["in Undercity Magic Quarter"] = "幽暗城魔法区内部"
-L["Jump to Skyhold"] = "前往苍穹要塞，跳！"
-L["Orb of translocation"] = "传送宝珠"
-L["Portal to Arathi Highlands"] = "阿拉希高地"
-L["Portal to Azsuna"] = "阿苏纳"
-L["Portal to Boralus"] = "伯拉勒斯"
-L["Portal to Caverns of Time"] = "时光之穴"
-L["Portal to Dalaran"] = "达拉然"
-L["Portal to Darkshore"] = "黑海岸"
-L["Portal to Darnassus"] = "达纳苏斯"
-L["Portal to Deepholm"] = "深岩之洲"
-L["Portal to Emerald Dreamway"] = "翡翠梦境"
-L["Portal to Exodar"] = "埃索达"
-L["Portal to Helheim"] = "冥狱深渊"
-L["Portal to Hellfire Peninsula"] = "地狱火半岛"
-L["Portal to Highmountain"] = "至高岭"
-L["Portal to Howling Fjord"] = "嚎风峡湾"
-L["Portal to Hyjal"] = "海加尔山"
-L["Portal to Ironforge"] = "铁炉堡"
-L["Portal to Isle of Quel'Danas"] = "奎尔丹纳斯岛"
-L["Portal to Isle of Thunder"] = "雷神岛"
-L["Portal to Jade Forest"] = "翡翠林"
-L["Portal to Lion's watch"] = "雄狮岗哨"
-L["Portal to Nazjatar"] = "纳沙塔尔"
-L["Portal to Orgrimmar"] = "奥格瑞玛"
-L["Portal to Peak of Serenity"] = "晴日峰（昆莱山）"
-L["Portal to Port of Boralus"] = "伯拉勒斯港口"
-L["Portal to Port of Zandalar"] = "赞达拉港口"
-L["Portal to Shado-Pan Garrison"] = "影踪卫戍营"
-L["Portal to Shattrath"] = "沙塔斯"
-L["Portal to Silithus"] = "希利苏斯"
-L["Portal to Silvermoon"] = "银月城"
-L["Portal to Stormheim"] = "风暴峡湾"
-L["Portal to Stormshield"] = "暴风之盾"
-L["Portal to Stormwind"] = "暴风城"
-L["Portal to Stranglethorn Vale"] = "荆棘谷"
-L["Portal to Suramar"] = "苏拉玛"
-L["Portal to Temple of Earth"] = "大地神殿"
-L["Portal to the Purple Parlor"] = "紫色天台"
-L["Portal to Therazane's Throne"] = "塞拉赞恩的王座"
-L["Portal to Thunder Bluff"] = "雷霆崖"
-L["Portal to Tol Barad"] = "托尔巴拉德"
-L["Portal to Twilight Highlands"] = "暮光高地"
-L["Portal to Uldum"] = "奥丹姆"
-L["Portal to Undercity"] = "幽暗城"
-L["Portal to Val'sharah"] = "瓦尔莎拉"
-L["Portal to Vashj'ir"] = "瓦斯琪尔"
-L["Portal to Vol'mar"] = "沃马尔"
-L["Portal to Warspear"] = "阿什兰"
+L["handler_tooltip_rep"] = "需要声望"
+L["handler_tooltip_TNTIER"] = "旅行网络的第 %s 层."
+L["handler_tooltip_not_available"] = "当前不可用"
+--L["currently available"] = "目前可用"
+
+----------------------------------------------------------------------------------------------------
+----------------------------------------------DATABASE----------------------------------------------
+----------------------------------------------------------------------------------------------------
+
+--==========================================SHADOWLANDS===========================================--
+
+L["Portal to Oribos"] = "前往奥利波斯"
+L["Waystone to Oribos"] = "通往奥利波斯的道标石"
+L["To Ring of Transference"] = "前往转移之环"
+L["To Ring of Fates"] = "前往命运之环"
+L["Into the Maw"] = "进入噬渊"
+L["To Keeper's Respite"] = "前往守护者的休憩"
+L["Portal to Torghast"] = "前往托加斯特"
+L["Portal to Zereth Mortis"] = "前往扎雷殁提斯"
+L["Portal to Karazhan"] = "前往卡拉赞"
+L["Portal to Mechagon"] = "前往麦卡贡"
+L["Portal to Gorgrond"] = "前往戈尔隆德"
+
+--============================================Bastion=============================================--
+
+L["Anima Gateway to Hero's rest"] = "通往英雄之眠传送门"
+
+-------------------------------------------------BfA------------------------------------------------
+
 L["Portal to Zuldazar"] = "祖达萨"
-L["Return to Boralus"] = "返回伯拉勒斯"
+L["Boat to Zuldazar"] = "前往祖达萨的船"
 L["Return to Zuldazar"] = "返回祖达萨"
+L["Boat to Vol'Dun"] = "前往沃顿的船"
+L["Boat to Nazmir"] = "前往纳兹米尔的船"
+L["Portal to Nazjatar"] = "纳沙塔尔"
 L["Submarine to Mechagon"] = "前往麦卡贡的潜艇"
+L["Portal to Silithus"] = "希利苏斯"
+
+L["Portal to Boralus"] = "伯拉勒斯"
+L["Boat to Boralus"] = "前往伯拉勒斯的船"
+L["Return to Boralus"] = "返回伯拉勒斯"
+L["Boat to Drustvar"] = "前往德鲁斯瓦的船"
+L["Boat to Stormsong Valley"] = "前往斯托颂谷地的船"
+L["Boat to Tiragarde Sound"] = "前往提拉加德海峡的船"
+
+L["Portal to Arathi Highlands"] = "前往塞拉摩的船"
+L["Portal to Port of Zandalar"] = "赞达拉港口"
+L["Portal to Darkshore"] = "黑海岸"
+L["Portal to Port of Boralus"] = "伯拉勒斯港口"
+
+-----------------------------------------------LEGION-----------------------------------------------
+
+L["Portal to Stormheim"] = "风暴峡湾"
+L["Portal to Helheim"] = "冥狱深渊"
+L["Portal to Dalaran"] = "达拉然"
+L["Portal to Azsuna"] = "阿苏纳"
+L["Portal to Val'sharah"] = "瓦尔莎拉"
+L["Portal to Emerald Dreamway"] = "翡翠梦境"
+L["Portal to Suramar"] = "苏拉玛"
+L["Portal to Highmountain"] = "至高岭"
+L["Great Eagle to Trueshot Lodge"] = "前往神射手营地的巨鹰"
+L["Jump to Skyhold"] = "前往苍穹要塞，跳！"
+
+-------------------------------------------------WoD------------------------------------------------
+
+L["Portal to Warspear"] = "阿什兰"
+L["Portal to Stormshield"] = "暴风之盾"
+L["Portal to Vol'mar"] = "沃马尔"
+L["Portal to Lion's watch"] = "雄狮岗哨"
+L["Ogre Waygate"] = "食人魔传送门"
+
+-------------------------------------------------MoP------------------------------------------------
+
+L["Portal to Jade Forest"] = "翡翠林"
+L["Portal to Pandaria"] = "潘达利亚传送门"
+L["Portal to Isle of Thunder"] = "雷神岛"
+L["Portal to Shado-Pan Garrison"] = "影踪卫戍营"
+L["Portal to Peak of Serenity"] = "晴日峰（昆莱山）"
+
+-------------------------------------------------CATA-----------------------------------------------
+
+L["Portal to Deepholm"] = "深岩之洲"
+L["Portal to Temple of Earth"] = "大地神殿"
+L["Portal to Therazane's Throne"] = "塞拉赞恩的王座"
+L["Portal to Twilight Highlands"] = "暮光高地"
+L["Portal to Tol Barad"] = "托尔巴拉德"
+L["Portal to Uldum"] = "奥丹姆"
+L["Portal to Vashj'ir"] = "瓦斯琪尔"
+L["Portal to Hyjal"] = "海加尔山"
+
+------------------------------------------------WotLK-----------------------------------------------
+
+L["Portal to the Purple Parlor"] = "紫色天台"
+L["Boat to Howling Fjord"] = "前往嚎风峡湾的船"
+L["Boat to Kamagua"] = "前往卡玛古（嚎风峡湾）的船"
+L["Portal to Howling Fjord"] = "嚎风峡湾"
+L["Boat to Borean Tundra"] = "前往北风苔原的船"
+L["Boat to Unu'Pe"] = "前往乌努比（北风苔原）的船"
 L["Zeppelin to Borean Tundra"] = "前往北风苔原的飞艇"
-L["Zeppelin to Orgrimmar"] = "前往奥格瑞玛的飞艇"
+L["Boat to Moa'Ki Harbor"] = "前往莫亚基港口（龙骨荒野）的船"
+L["Waygate to Sholazar Basin"] = "通往索拉查盆地"
+
+-------------------------------------------------BC-------------------------------------------------
+
+L["Portal to Hellfire Peninsula"] = "地狱火半岛"
+L["Portal to Shattrath"] = "沙塔斯"
+L["Portal to Isle of Quel'Danas"] = "奎尔丹纳斯岛"
+L["Portal to Exodar"] = "埃索达"
+L["in Exodar"] = "埃索达内部"
+L["Boat to Exodar"] = "前往埃索达的船"
+L["Speak with Zephyr"] = "和塞菲尔对话"
+
+-----------------------------------------------VANILLA----------------------------------------------
+
+L["Boat to Menethil Harbor"] = "前往米奈希尔港（湿地）的船"
+
+L["Portal to Silvermoon"] = "银月城"
+
+L["Portal to Undercity"] = "幽暗城"
+L["Orb of translocation"] = "传送宝珠"
+L["in Undercity Magic Quarter"] = "幽暗城魔法区内部"
+
 L["Zeppelin to Stranglethorn Vale"] = "前往荆棘谷的飞艇"
+L["Portal to Stranglethorn Vale"] = "荆棘谷"
+L["Boat to Booty Bay"] = "前往藏宝海湾的船"
+
+L["Portal to Stormwind"] = "暴风城"
+L["Boat to Stormwind"] = "前往暴风城的船"
+L["Deeprun Tram to Stormwind"] = "前往暴风城的矿道地铁"
+
+L["Portal to Ironforge"] = "铁炉堡"
+L["Deeprun Tram to Ironforge"] = "前往铁炉堡的矿道地铁"
+
+L["Portal to Orgrimmar"] = "奥格瑞玛"
+L["Zeppelin to Orgrimmar"] = "前往奥格瑞玛的飞艇"
+
+L["Portal to Thunder Bluff"] = "雷霆崖"
 L["Zeppelin to Thunder Bluff"] = "前往雷霆崖的飞艇"
+
+L["Portal to Darnassus"] = "达纳苏斯"
+
+L["Boat to Ratchet"] = "前往棘齿城（北贫瘠之地）的船"
+
+L["Boat to Theramore"] = "前往塞拉摩的船"
+
+L["Portal to Caverns of Time"] = "时光之穴"
+
+L["Portal to Dalaran Crater"] = "达拉然巨坑"
+L["Portal to the Sepulcher"] = "墓地"
+
+L["Waygate to Un'Goro Crater"] = "通往安戈洛环形山"
 end
