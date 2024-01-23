1a211f05d35609a7ec5650a90e7354de25d90965
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-01-23 10:57:33 +0800

    Update zhCN.lua

--- Retail/localization/zhCN.lua
+++ Retail/localization/zhCN.lua
@@ -3,7 +3,7 @@ local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_TravelGuide", "zhCN", f
 if not L then return end
 -- Simplified Chinese translation by mmk5 ( https://www.curseforge.com/members/mmk5 )
 --  zhCN client: (NGA-男爵凯恩)
---  Last update: 2023/11/17
+--  Last update: 2024/01/23
 if L then
 ----------------------------------------------------------------------------------------------------
 -----------------------------------------------CONFIG-----------------------------------------------
@@ -138,105 +138,105 @@ L["Portal to Emerald Dream"] = "通往翡翠梦境的传送门"
 L["Portal to Ohn'ahran Plains"] = "通往欧恩哈拉平原的传送门"
 L["Portal to Central Encampment"] = "通往中心营地的传送门"
 L["Portal to The Timeways"] = "通往时间流的传送门"
--- L["Portal to Bel'ameth"] = ""
--- L["Portal to Feathermoon Stronghold"] = ""
+L["Portal to Bel'ameth"] = "通往贝拉梅斯的传送门"
+L["Portal to Feathermoon Stronghold"] = "通往羽月要塞的传送门"
 
 --==========================================SHADOWLANDS===========================================--
 
-L["Portal to Oribos"] = "前往奥利波斯"
+L["Portal to Oribos"] = "通往奥利波斯的传送门"
 L["Waystone to Oribos"] = "通往奥利波斯的道标石"
 L["To Ring of Transference"] = "前往转移之环"
 L["To Ring of Fates"] = "前往命运之环"
 L["Into the Maw"] = "进入噬渊"
-L["To Keeper's Respite"] = "前往守护者的休憩"
-L["Portal to Torghast"] = "前往托加斯特"
-L["Portal to Zereth Mortis"] = "前往扎雷殁提斯"
-L["Portal to Karazhan"] = "前往卡拉赞"
-L["Portal to Mechagon"] = "前往麦卡贡"
-L["Portal to Gorgrond"] = "前往戈尔隆德"
+L["To Keeper's Respite"] = "前往守护者的休憩的传送门"
+L["Portal to Torghast"] = "通往托加斯特的传送门"
+L["Portal to Zereth Mortis"] = "通往扎雷殁提斯的传送门"
+L["Portal to Karazhan"] = "通往卡拉赞的传送门"
+L["Portal to Mechagon"] = "通往麦卡贡的传送门"
+L["Portal to Gorgrond"] = "通往戈尔隆德的传送门"
 
 --============================================Bastion=============================================--
 
-L["Anima Gateway to Hero's rest"] = "通往英雄之眠传送门"
+L["Anima Gateway to Hero's rest"] = "心能传送门"
 
 -------------------------------------------------BfA------------------------------------------------
 
-L["Portal to Zuldazar"] = "通往祖达萨"
+L["Portal to Zuldazar"] = "通往祖达萨的传送"
 L["Boat to Zuldazar"] = "前往祖达萨的船"
 L["Return to Zuldazar"] = "返回祖达萨"
 L["Boat to Vol'dun"] = "前往沃顿的船"
 L["Boat to Nazmir"] = "前往纳兹米尔的船"
-L["Portal to Nazjatar"] = "通往纳沙塔尔"
+L["Portal to Nazjatar"] = "通往纳沙塔尔的传送门"
 L["Submarine to Mechagon"] = "前往麦卡贡的潜艇"
-L["Portal to Silithus"] = "通往希利苏斯"
+L["Portal to Silithus"] = "通往希利苏斯的传送门"
 L["Boat to Echo Isles"] = "通往回声群岛的船"
 
-L["Portal to Boralus"] = "通往伯拉勒斯"
-L["Boat to Boralus"] = "前往伯拉勒斯的船"
+L["Portal to Boralus"] = "通往伯拉勒斯的传送门"
+L["Boat to Boralus"] = "前往伯拉勒斯（提拉加德海峡）的船"
 L["Return to Boralus"] = "返回伯拉勒斯"
 L["Boat to Drustvar"] = "前往德鲁斯瓦的船"
 L["Boat to Stormsong Valley"] = "前往斯托颂谷地的船"
 L["Boat to Tiragarde Sound"] = "前往提拉加德海峡的船"
 
-L["Portal to Arathi Highlands"] = "前往塞拉摩的船"
-L["Portal to Port of Zandalar"] = "通往赞达拉港口"
-L["Portal to Darkshore"] = "通往黑海岸"
-L["Portal to Port of Boralus"] = "通往伯拉勒斯港口"
+L["Portal to Arathi Highlands"] = "前往塞拉摩的传送门"
+L["Portal to Port of Zandalar"] = "通往赞达拉的传送门"
+L["Portal to Darkshore"] = "通往黑海岸的传送门"
+L["Portal to Port of Boralus"] = "通往伯拉勒斯的传送门"
 
 -----------------------------------------------LEGION-----------------------------------------------
 
-L["Portal to Stormheim"] = "通往风暴峡湾"
-L["Portal to Helheim"] = "通往冥狱深渊"
-L["Portal to Dalaran"] = "通往达拉然"
-L["Portal to Azsuna"] = "通往阿苏纳"
-L["Portal to Val'sharah"] = "通往瓦尔莎拉"
-L["Portal to Emerald Dreamway"] = "通往翡翠梦境"
-L["Portal to Suramar"] = "通往苏拉玛"
-L["Portal to Highmountain"] = "通往至高岭"
+L["Portal to Stormheim"] = "通往风暴峡湾的传送门"
+L["Portal to Helheim"] = "通往冥狱深渊的传送门"
+L["Portal to Dalaran"] = "通往达拉然（晶歌森林）的传送门"
+L["Portal to Azsuna"] = "通往阿苏纳的传送门"
+L["Portal to Val'sharah"] = "通往瓦尔莎拉的传送门"
+L["Portal to Emerald Dreamway"] = "通往翡翠梦境的传送门"
+L["Portal to Suramar"] = "通往苏拉玛的传送门"
+L["Portal to Highmountain"] = "通往至高岭的传送门"
 L["Great Eagle to Trueshot Lodge"] = "前往神射手营地的巨鹰"
 L["Jump to Skyhold"] = "前往苍穹要塞，跳！"
-L["Portal to Duskwood"] = "通往暮色森林"
-L["Portal to Feralas"] = "通往菲拉斯"
-L["Portal to Grizzly Hills"] = "通往灰熊丘陵"
-L["Portal to Hinterlands"] = "通往辛特兰"
-L["Portal to Moonglade"] = "通往月光林地"
-L["Portal to Dreamgrove"] = "通往梦境林地"
+L["Portal to Duskwood"] = "通往暮色森林的传送门"
+L["Portal to Feralas"] = "通往菲拉斯的传送门"
+L["Portal to Grizzly Hills"] = "通往灰熊丘陵的传送门"
+L["Portal to Hinterlands"] = "通往辛特兰的传送门"
+L["Portal to Moonglade"] = "通往月光林地的传送门"
+L["Portal to Dreamgrove"] = "通往梦境林地的传送门"
 
 -------------------------------------------------WoD------------------------------------------------
 
-L["Portal to Warspear"] = "通往阿什兰"
-L["Portal to Stormshield"] = "通往暴风之盾"
-L["Portal to Vol'mar"] = "通往沃马尔"
-L["Portal to Lion's watch"] = "通往雄狮岗哨"
+L["Portal to Warspear"] = "通往阿什兰的传送门"
+L["Portal to Stormshield"] = "通往暴风之盾（阿什兰）的传送门"
+L["Portal to Vol'mar"] = "通往沃马尔的传送门"
+L["Portal to Lion's watch"] = "通往雄狮岗哨的传送门"
 L["Ogre Waygate"] = "食人魔传送门"
 L["Reflective Portal"] = "反射传送门[百变之镜]"
 
 -------------------------------------------------MoP------------------------------------------------
 
-L["Portal to Jade Forest"] = "通往翡翠林"
-L["Portal to Pandaria"] = "潘达利亚传送门"
-L["Portal to Isle of Thunder"] = "通往雷神岛"
-L["Portal to Shado-Pan Garrison"] = "通往影踪卫戍营"
-L["Portal to Peak of Serenity"] = "通往晴日峰（昆莱山）"
+L["Portal to Jade Forest"] = "通往翡翠林的传送门"
+L["Portal to Pandaria"] = "通往潘达利亚的传送门"
+L["Portal to Isle of Thunder"] = "通往雷神岛的传送门"
+L["Portal to Shado-Pan Garrison"] = "通往影踪卫戍营的传送门"
+L["Portal to Peak of Serenity"] = "通往晴日峰（昆莱山）的传送门"
 
 -------------------------------------------------CATA-----------------------------------------------
 
-L["Portal to Deepholm"] = "通往深岩之洲"
-L["Portal to Temple of Earth"] = "通往大地神殿"
-L["Portal to Therazane's Throne"] = "塞拉赞恩的王座"
-L["Portal to Twilight Highlands"] = "通往暮光高地"
-L["Portal to Tol Barad"] = "通往托尔巴拉德"
-L["Portal to Uldum"] = "通往奥丹姆"
-L["Portal to Vashj'ir"] = "通往瓦斯琪尔"
-L["Portal to Hyjal"] = "通往海加尔山"
+L["Portal to Deepholm"] = "通往深岩之洲的传送门"
+L["Portal to Temple of Earth"] = "通往大地神殿的传送门"
+L["Portal to Therazane's Throne"] = "通往塞拉赞恩的王座的传送门"
+L["Portal to Twilight Highlands"] = "通往暮光高地的传送门"
+L["Portal to Tol Barad"] = "通往托尔巴拉德的传送门"
+L["Portal to Uldum"] = "通往奥丹姆的传送门"
+L["Portal to Vashj'ir"] = "通往瓦丝琪尔的传送门"
+L["Portal to Hyjal"] = "通往海加尔的传送门"
 
 ------------------------------------------------WotLK-----------------------------------------------
 
-L["Portal to the Purple Parlor"] = "紫色天台"
+L["Portal to the Purple Parlor"] = "通往紫色天台的传送门"
 L["Boat to Howling Fjord"] = "前往嚎风峡湾的船"
 L["Boat to Kamagua"] = "前往卡玛古（嚎风峡湾）的船"
-L["Portal to Howling Fjord"] = "通往嚎风峡湾"
-L["Boat to Borean Tundra"] = "前往北风苔原的船"
+L["Portal to Howling Fjord"] = "通往嚎风峡湾的传送门"
+L["Boat to Borean Tundra"] = "前往无畏要塞（北风苔原）的船"
 L["Boat to Unu'pe"] = "前往乌努比（北风苔原）的船"
 L["Zeppelin to Borean Tundra"] = "前往北风苔原的飞艇"
 L["Boat to Moa'ki Harbor"] = "前往莫亚基港口（龙骨荒野）的船"
@@ -244,51 +244,51 @@ L["Waygate to Sholazar Basin"] = "通往索拉查盆地"
 
 -------------------------------------------------BC-------------------------------------------------
 
-L["Portal to Hellfire Peninsula"] = "通往地狱火半岛"
-L["Portal to Shattrath"] = "通往沙塔斯"
-L["Portal to Isle of Quel'Danas"] = "通往奎尔丹纳斯岛"
-L["Portal to Exodar"] = "通往埃索达"
+L["Portal to Hellfire Peninsula"] = "通往地狱火半岛的传送门"
+L["Portal to Shattrath"] = "通往沙塔斯的传送门"
+L["Portal to Isle of Quel'Danas"] = "通往奎尔丹纳斯岛的传送门"
+L["Portal to Exodar"] = "通往埃索达的传送门"
 L["in Exodar"] = "埃索达内部"
 L["Boat to Exodar"] = "前往埃索达的船"
-L["Speak with Zephyr"] = "和塞菲尔对话"
+L["Speak with Zephyr"] = "和塞菲尔对话-传送"
 
 -----------------------------------------------VANILLA----------------------------------------------
 
 L["Boat to Menethil Harbor"] = "前往米奈希尔港（湿地）的船"
 
-L["Portal to Silvermoon"] = "通往银月城"
+L["Portal to Silvermoon"] = "通往银月城的传送门"
 
-L["Portal to Undercity"] = "通往幽暗城"
+L["Portal to Undercity"] = "通往幽暗城的传送门"
 L["Orb of translocation"] = "传送宝珠"
 L["in Undercity Magic Quarter"] = "幽暗城魔法区内部"
 
 L["Zeppelin to Stranglethorn Vale"] = "前往荆棘谷的飞艇"
-L["Portal to Stranglethorn Vale"] = "通往荆棘谷"
+L["Portal to Stranglethorn Vale"] = "通往荆棘谷的传送门"
 L["Boat to Booty Bay"] = "前往藏宝海湾的船"
 
-L["Portal to Stormwind"] = "通往暴风城"
+L["Portal to Stormwind"] = "通往暴风城的传送门"
 L["Boat to Stormwind"] = "前往暴风城的船"
 L["Deeprun Tram to Stormwind"] = "前往暴风城的矿道地铁"
 
-L["Portal to Ironforge"] = "通往铁炉堡"
+L["Portal to Ironforge"] = "通往铁炉堡的传送门"
 L["Deeprun Tram to Ironforge"] = "前往铁炉堡的矿道地铁"
 
-L["Portal to Orgrimmar"] = "通往奥格瑞玛"
+L["Portal to Orgrimmar"] = "通往奥格瑞玛的传送门"
 L["Zeppelin to Orgrimmar"] = "前往奥格瑞玛的飞艇"
 
-L["Portal to Thunder Bluff"] = "通往雷霆崖"
+L["Portal to Thunder Bluff"] = "通往雷霆崖的传送门"
 L["Zeppelin to Thunder Bluff"] = "前往雷霆崖的飞艇"
 
-L["Portal to Darnassus"] = "通往达纳苏斯"
+L["Portal to Darnassus"] = "通往达纳苏斯的传送门"
 
 L["Boat to Ratchet"] = "前往棘齿城（北贫瘠之地）的船"
 
 L["Boat to Theramore Isle"] = "前往塞拉摩的船"
 
-L["Portal to Caverns of Time"] = "通往时光之穴"
+L["Portal to Caverns of Time"] = "通往时光之穴的传送门"
 
-L["Portal to Dalaran Crater"] = "通往达拉然巨坑"
-L["Portal to the Sepulcher"] = "墓地"
+L["Portal to Dalaran Crater"] = "通往达拉然巨坑的传送门"
+L["Portal to the Sepulcher"] = "通往墓地的传送门"
 
 L["Waygate to Un'Goro Crater"] = "通往安戈洛环形山"
 L["The Masonary"] = "石匠区"
