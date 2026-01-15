97c25832190057b3c23fcc5f3affa516fbfb1cbe
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2026-01-16 00:23:42 +0500

    Update RU locale (#87)
    * Update HandyNotes_TravelGuide.toc
    
    * Update HandyNotes_TravelGuide.toc
    
    * Update ruRU.lua

--- Retail/localization/ruRU.lua
+++ Retail/localization/ruRU.lua
@@ -256,9 +256,9 @@ L["Portal to Isle of Thunder"] = "Портал на Остров Грома"
 L["Portal to Shado-Pan Garrison"] = "Портал в Гарнизон Шадо-Пан"
 L["Portal to Shan'ze Dao"] = "Портал на Шань'цзэ Дао"
 L["Portal to Peak of Serenity"]= "Портал на Вершину Кунь-Лай"
--- L["Hot Air Balloon to Orgrimmar"] = ""
--- L["Hot Air Balloon to Stormwind"] = ""
--- L["Hot Air Balloon to Wandering Isle"] = ""
+L["Hot Air Balloon to Orgrimmar"] = "Воздушный шар в Оргриммар"
+L["Hot Air Balloon to Stormwind"] = "Воздушный шар в Штормград"
+L["Hot Air Balloon to Wandering Isle"] = "Воздушный шар на Скитающийся остров"
 
 -------------------------------------------------CATA-----------------------------------------------
 
